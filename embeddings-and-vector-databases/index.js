538f514c1d1b6911087a9463792da1ed91497a94
Sachini <sachichathudissanayaka@gmail.com>
2024-11-14 22:03:19 +0530

    Add changes

---
+++ embeddings-and-vector-databases/index.js
@@ -0,0 +1,32 @@
+import openai from './config.js';
+
+const content = [
+  "Beyond Mars: speculating life on distant planets.",
+  "Jazz under stars: a night in New Orleans' music scene.",
+  "Mysteries of the deep: exploring uncharted ocean caves.",
+  "Rediscovering lost melodies: the rebirth of vinyl culture.",
+  "Tales from the tech frontier: decoding AI ethics.",
+]; 
+
+/*
+  Challenge: Pair text with its embedding
+    - For each text input, create an object with 
+      a 'content' and 'embedding' property
+    - The value of 'content' should be the text
+    - The value of 'embedding' should be the vector embedding for that text
+*/
+
+async function main(input) {
+  await Promise.all(
+    input.map( async(textChunk) => {
+        const embeddingResponse = await openai.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: textChunk,
+        });
+        const data = { content: textChunk, embedding: embeddingResponse.data[0].embedding }
+        console.log(data);  
+    })    
+  );
+  console.log('Embedding complete!');
+}
+main(content);
