d58d4a1e74658ca125c5c560cadb32866b0e4fe4
Sachini <sachichathudissanayaka@gmail.com>
2025-04-01 13:34:53 +0530

    Add changes

--- embeddings-and-vector-databases/pair-text-with-embedding/config.js
+++ embeddings-and-vector-databases/pair-text-with-embedding/config.js
@@ -1,11 +1,9 @@
 import OpenAI from 'openai';
 
-/** Ensure the OpenAI API key is available and correctly configured */
 if (!import.meta.env.VITE_OPENAI_API_KEY) {
     throw new Error("OpenAI API key is missing or invalid.");
 }
 
-/** OpenAI config */
 export default new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
